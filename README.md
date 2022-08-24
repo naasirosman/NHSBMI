2eea73e611f69c276e7dac64fb19f144f79992a9
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-08-24 20:56:01 +0100

    complete

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NHSBMI
+
+How the BMI calculator works (JavaScript):
+
+- once the "calculate" is clicked, it fetches what is in the feet, inches, weight fields and applies the BMI equation to it
+- there are if statements to ensure that the user inputs a valid height, weight, and age (as well as ensuring adult tab is not used for u18s and child tab not used for under 2s and over 18s)
+
+Improvements to be made:
+- the adult/child toggle doesn't work: the solution to this is to add and remove style classes but theres a bug with this
+- date input accepts invalid dates on months with less than 31 days e.g. April 31st, June 31st, February 30th
+- age is not used in the BMI calculation
+- all fields are not mandatory
--- nhs.js
+++ nhs.js
@@ -20,18 +20,45 @@
 //     console.log(inches)
 // }
 
-
+let heightUnit = "feet";
+let weightUnit = "kg"
+console.log(heightUnit)
+console.log(weightUnit)
 
 
 function calculate() {
+   console.log(heightUnit)
 
     var inches = parseInt(document.querySelector('.inchbox').value); 
     var feet = parseInt(document.querySelector('.feetbox').value);
-    var weight = parseInt(document.querySelector('.kgbox').value)
-     var age = parseInt(document.querySelector('.agebox').value)
+    var kg = parseInt(document.querySelector('.kgbox').value)
+    var age = parseInt(document.querySelector('.agebox').value)
+    var cm = parseInt(document.querySelector('.cmBox').value)
+    var lbs = parseInt(document.querySelector('.lbsBox').value)
+    var day = parseInt(document.querySelector('.day').value)
+    var month = parseInt(document.querySelector('.month').value)
+    var year = parseInt(document.querySelector('.year').value)
 
-    const result = weight / (((((feet * 12) + inches) * 2.54) / 100) ** 2)
-    console.log(result)
+    let result
+
+    if (heightUnit === "feet" && weightUnit === "kg") {
+      console.log(`height = ft, in =  ${feet} ' ${inches}... weight = kg = ${kg}`  )
+      result = kg / (((((feet * 12) + inches) * 2.54) / 100) ** 2)
+      console.log(result)
+    } else if (heightUnit === "cm" && weightUnit === "kg") {
+      console.log(`height = cm =  ${cm}... weight = kg = ${kg}`  )
+        result = kg / ((cm / 100) ** 2)
+        console.log(result)
+    }
+    else if (heightUnit === "feet" && weightUnit === "lbs") {
+      console.log(`height = ft, in =  ${feet} ' ${inches}... weight = lbs = ${lbs}`  )
+      result = ( lbs * 0.45 ) / (((((feet * 12) + inches) * 2.54) / 100) ** 2)
+      console.log(result)
+    } else {
+      console.log(`height = cm =  ${cm}... weight = lbs = ${lbs}`  )
+      result = ( lbs * 0.45 ) / ((cm / 100) ** 2)
+      console.log(result)
+    };
 
     if (feet >= 8 || feet <= 3) { 
         const heightError = "This height is invalid!"
@@ -45,7 +72,8 @@ function calculate() {
         document.querySelector('.heighterror').style.display = "block";
         return;        
      }
-     if (weight >= 201 || weight <= 20) { 
+     if (kg >= 201 || kg <= 20) { 
+
         const heightError = "This weight is invalid!"
         console.log(heightError);
         document.querySelector('.weighterror').style.display = "block";
@@ -58,34 +86,104 @@ function calculate() {
         return;        
      }
      if (age <= 18) { 
-        const heightError = "This age is invalid!"
-        console.log(heightError);
+        const ageError = "This age is invalid!"
+        console.log(ageError);
         document.querySelector('.age-child-error').style.display = "block";
         return;        
      }
+     if (day <= 0 || day >= 32) {
+      const heightError = "This age is invalid!"
+      console.log(heightError);
+      document.querySelector('.age-child-error').style.display = "block";
+      document.querySelector('.age-child-error').innerHTML = "Enter a valid date";
+      return;  
+     }
+     if (month <= 0 || day >= 13) {
+      const heightError = "This age is invalid!"
+      console.log(heightError);
+      document.querySelector('.age-child-error').style.display = "block";
+      document.querySelector('.age-child-error').innerHTML = "Enter a valid date";
+      return;  
+     }
+     if (year <= 2004) {
+      const heightError = "This age is invalid!"
+      console.log(heightError);
+      document.querySelector('.age-child-error').style.display = "block";
+      document.querySelector('.age-child-error').innerHTML = "This person is not a child";
+      return;  
+     }
+     if (year > 2020) {
+      const heightError = "This age is invalid!"
+      console.log(heightError);
+      document.querySelector('.age-child-error').style.display = "block";
+      document.querySelector('.age-child-error').innerHTML = "This person is too young for BMI to be measured";
+      return;  
+     }
 }
-const switchToCm = document.querySelector(".cmlink")
-switchToCm.addEventListener("click", function() {
+const feetAndCmToggle = document.querySelector(".cmlink")
+feetAndCmToggle.addEventListener("click", function() {
+   if (heightUnit === "feet") {
   document.querySelector(".centimetres").style.display = "block"
   document.querySelector(".inches").style.display = "none"
-  document.querySelector(".feet").style.display = "none"});
+  document.querySelector(".feet").style.display = "none"
+  document.querySelector(".cmlink").innerHTML = "Switch to ft, in"
+   heightUnit = "cm"   
+   } else if (heightUnit === "cm") {   
+   document.querySelector(".centimetres").style.display = "none"
+   document.querySelector(".inches").style.display = "block"
+   document.querySelector(".feet").style.display = "block"
+   document.querySelector(".cmlink").innerHTML = "Switch to cm"
+    heightUnit = "feet"}
+    else {
+      document.querySelector(".centimetres").style.display = "none"
+      document.querySelector(".inches").style.display = "none"
+      document.querySelector(".feet").style.display = "none"
+    }
+});
 
 
-const switchToLbs = document.querySelector(".lbs-link-div")
-    switchToLbs.addEventListener("click", function() {
-      document.querySelector(".lbs").style.display = "block"
-      document.querySelector(".kg").style.display = "none"});
-
+const kgAndLbsToggle = document.querySelector(".lbslink")
+kgAndLbsToggle.addEventListener("click", function() {
+   if (weightUnit === "kg") {
+  document.querySelector(".lbs").style.display = "block"
+  document.querySelector(".kg").style.display = "none"
+  document.querySelector(".lbslink").innerHTML = "Switch to kg" 
+   weightUnit = "lbs"}
+  else if (weightUnit === "lbs") {
+   document.querySelector(".lbs").style.display = "block"
+   document.querySelector(".kg").style.display = "none"
+   document.querySelector(".lbslink").innerHTML = "Switch to lbs" }
+   else {
+      document.querySelector(".lbs").style.display = "none"
+      document.querySelector(".kg").style.display = "none" 
+   }
+});
 
 
       const switchToChild = document.querySelector(".child")
       switchToChild.addEventListener("click", function() {
-        document.querySelector(".child").style.backgroundColor = "#215eb8"
-      //   document.querySelector(".child").style:hover. = "#215eb8"
-        document.querySelector(".adult").className = "adultWhenChildIsSelected"
-      //   document.querySelector(".adult").classList.remove = ("hoverchild")
-      //   document.querySelector(".adult").style.di = "none"
-   });
+         document.querySelector(".adult").classList.remove("chosen")
+        document.querySelector(".child").classList.add("chosen")
+        document.querySelector(".adult").classList.add("unchosen")
+        document.querySelector(".child").classList.remove("unchosen")
+        document.querySelector(".childAge").style.display = "block"
+        document.querySelector(".agebox").style.display = "none"
+        ageGroup = "child"
+   }); 
+   const switchToAdult = document.querySelector(".adult");
+   console.log(switchToAdult);
+   switchToAdult.addEventListener("click", function() { 
+
+      document.querySelector(".adult").classList.remove("unchosen");
+      document.querySelector(".child").classList.add("unchosen");
+      document.querySelector(".adult").classList.add("chosen");
+      document.querySelector(".child").classList.remove("chosen");
+      document.querySelector(".childAge").style.display = "none"
+      document.querySelector(".agebox").style.display = "block"
+     ageGroup = "adult"
+});
+
+
 
 // console.log(calculateBmi(12))
 
